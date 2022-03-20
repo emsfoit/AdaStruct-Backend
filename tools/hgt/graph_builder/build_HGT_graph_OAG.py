2c239ed85c281e718d6d750547441de636235c5b
Moe <emsfoit@gmail.com>
2022-03-20 18:51:11 +0100

    error handling

--- tools/hgt/graph_builder/build_HGT_graph_OAG.py
+++ tools/hgt/graph_builder/build_HGT_graph_OAG.py
@@ -1,10 +1,13 @@
 from tools.hgt.pyHGT.datax import HGTGraph
 import networkx as nx
+import traceback
 
-def build_hgt_graph(graph_setting, data, output_graph_file):
+def build_hgt_graph(graph_setting, data, output_graph_file, logger):
     try:
-        OAG_HGT_graph = HGTGraph(graph_setting, data)
+        OAG_HGT_graph = HGTGraph(graph_setting, data, logger)
         nx.write_gpickle(OAG_HGT_graph.G, output_graph_file)
         return True
-    except:
+    except Exception as e:
+        logger(e)
+        logger(traceback.format_exc())
         return False
